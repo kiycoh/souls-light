34393edb6fff07fa42f77c92d079f940a7aa15f8
DarvX92 <dario.marceno.92@.com>
2025-12-17 17:07:16 +0100

    Prima parte dei test della mappa

---
+++ core/src/main/java/io/github/soulslight/model/Maps/ConcreteRoomBuilder.java
@@ -0,0 +1,33 @@
+package io.github.soulslight.model.Maps;
+
+public class ConcreteRoomBuilder implements RoomBuilder {
+    private Room result;
+
+    public ConcreteRoomBuilder() {
+        this.reset();
+    }
+
+    @Override
+    public void reset() {
+        this.result = new Room();
+    }
+
+    @Override
+    public void loadTemplate(String templateName) {
+        // Logica per settare il nome del template nel prodotto
+        this.result.setTemplateName(templateName);
+        // qui si caricheranno i template in .json
+    }
+
+    @Override
+    public Room getResult() {
+        return this.result;
+    }
+
+    @Override
+    public void addEnemies(String type) {
+        if (this.result == null) reset();
+        this.result.addEnemy(type);
+    }
+}
+
---
+++ core/src/main/java/io/github/soulslight/model/Maps/RoomBuilder.java
@@ -0,0 +1,11 @@
+package io.github.soulslight.model.Maps;
+
+public interface RoomBuilder {
+    void reset();
+    void loadTemplate(String templateName);
+
+    void addEnemies(String type);
+    // Qua si dovrebbero aggiungere anche gli altri metodi dei diagrammi (void setDoors(), void addEnemies(String type), ecc)
+
+    Room getResult();
+}
