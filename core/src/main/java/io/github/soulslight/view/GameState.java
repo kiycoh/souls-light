6368bda3b599d5ab0e84aea46f33bbe10e04c3c9
kiycoh <alessandrocarosia@proton.me>
2026-01-15 20:30:48 +0100

    refactor: remove unused code from SoulsLightGame and added GameState interface for screen management

---
+++ core/src/main/java/io/github/soulslight/view/GameState.java
@@ -0,0 +1,10 @@
+package io.github.soulslight.view;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * Pattern: State Sealed interface managing the screen transitions and states. Implementations must
+ * be final because of sealed hierarchy.
+ */
+public sealed interface GameState extends Screen
+    permits SplashScreen, MainMenuScreen, GameScreen, SettingsScreen {}
